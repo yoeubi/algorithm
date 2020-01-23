71b06a8b44a5a600277159211b1efb273c83f1ae
yoeubi <yoeubi28@gmail.com>
2020-01-23 23:46:33 +0900

    oneday algorithm

---
+++ leetcode/convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+var getDecimalValue = function(head) {
+  let node = head;
+  let result = [];
+  while (node) {
+    result.push(node.val);
+    node = node.next;
+  }
+  return result.reduce((prev, node, index, arr) => {
+    console.log(prev, node, Math.pow(2, arr.length - index - 1), arr);
+    return prev + node * Math.pow(2, arr.length - index - 1);
+  }, 0);
+};
+
+console.log([1, 0, 1]);
+console.log();
+console.log();
+console.log();
+console.log();
