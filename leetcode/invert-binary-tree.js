6091dc3ba09d9a429d4513f88f33a7fe5f31a973
yoeubi <yoeubi28@gmail.com>
2020-02-27 17:55:12 +0900

    oneday algorithm

---
+++ leetcode/invert-binary-tree.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/invert-binary-tree/
+var invertTree = function(root) {
+  function traverse(node) {
+    if (node == null) return;
+    if (node.left && node.right) {
+      const temp = node.left;
+      node.left = node.right;
+      node.right = temp;
+      traverse(node.left);
+      traverse(node.right);
+    } else if (node.left) {
+      node.right = node.left;
+      node.left = null;
+      traverse(node.right);
+    } else {
+      node.left = node.right;
+      node.right = null;
+      traverse(node.left);
+    }
+  }
+  traverse(root);
+  return root;
+};
