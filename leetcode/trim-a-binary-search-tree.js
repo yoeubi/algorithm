6f807924694d2dab0b8f89a709accc8a1e78c380
yoeubi <yoeubi28@gmail.com>
2020-03-05 00:30:16 +0900

    oneday algorithm

---
+++ leetcode/trim-a-binary-search-tree.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/trim-a-binary-search-tree/
+var trimBST = function(root, L, R) {
+  if (root == null) return root;
+  if (root.val > R) return trimBST(root.left, L, R);
+  if (root.val < L) return trimBST(root.right, L, R);
+  root.left = trimBST(root.left, L, R);
+  root.right = trimBST(root.right, L, R);
+  return root;
+};
