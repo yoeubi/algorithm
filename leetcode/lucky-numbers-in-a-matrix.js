b57375163bee20eb4aacfe88c2e62ae3ec4c257c
yoeubi <yoeubi28@gmail.com>
2020-03-17 01:04:30 +0900

    oneday algorithm

--- leetcode/lucky-numbers-in-a-matrix.js
+++ leetcode/lucky-numbers-in-a-matrix.js
@@ -1,2 +1,23 @@
 // https://leetcode.com/problems/lucky-numbers-in-a-matrix/
-var luckyNumbers = function(matrix) {};
+var luckyNumbers = function(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let min = matrix[i][0];
+    let minIndex = 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      const sel = matrix[i][j];
+      if (min > sel) {
+        min = sel;
+        minIndex = j;
+      }
+    }
+    let max = min;
+    for (let j = 0; j < matrix.length; j++) {
+      max = Math.max(max, matrix[j][minIndex]);
+    }
+    if (min === max) {
+      result.push(min);
+    }
+  }
+  return result;
+};
