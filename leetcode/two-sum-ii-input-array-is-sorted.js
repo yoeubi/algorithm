f197d946b7a023dfa7dac29b5bdbcf4fadca47ab
yoeubi <yoeubi28@gmail.com>
2020-04-02 23:42:03 +0900

    oneday algorithm

--- leetcode/two-sum-ii-input-array-is-sorted.js
+++ leetcode/two-sum-ii-input-array-is-sorted.js
@@ -13,3 +13,15 @@ var twoSum = function(numbers, target) {
     }
     return [];
 };
+
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while(true) {
+        const sum = numbers[left] + numbers[right];
+        if(sum === target) return [left + 1, right + 1];
+        if(target > sum) left++;
+        else right--;
+    }
+    return [];
+};
