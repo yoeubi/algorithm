a5a02e463aa3d31dc2005d09a328f0e504ba3044
yoeubi <yoeubi28@gmail.com>
2020-04-17 22:12:57 +0900

    oneday algorithm

---
+++ leetcode/find-bottom-left-tree-value.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/find-bottom-left-tree-value/
+var findBottomLeftValue = function (root) {
+  let val = root.val;
+  let height = 0;
+  function traverse(node, dep) {
+    if (!node) return;
+    traverse(node.left, dep + 1);
+    if (dep > height) {
+      val = node.val;
+      height = dep;
+    }
+    traverse(node.right, dep + 1);
+  }
+  traverse(root, true, 0);
+  return val;
+};
