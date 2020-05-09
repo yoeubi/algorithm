b41f80a493e611eafbd7ccd4b4fc7956e33b62c7
yoeubi <yoeubi28@gmail.com>
2020-05-09 23:23:42 +0900

    oneday algorithm

--- leetcode/reverse-linked-list.js
+++ leetcode/reverse-linked-list.js
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/reverse-linked-list/
-var reverseList = function(head) {
+var reverseList = function (head) {
   let node = head;
   const arr = [];
   while (node) {
@@ -14,4 +14,16 @@ var reverseList = function(head) {
     node = node.next;
   }
   return head;
+};
+
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr != null) {
+    const nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
+  return prev;
 };
