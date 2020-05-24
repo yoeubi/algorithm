83057fd36d33a67f3334b367ed56ce06e2d954f0
yoeubi <yoeubi28@gmail.com>
2020-05-24 20:24:51 +0900

    oneday algorithm

---
+++ leetcode/12903.js
@@ -0,0 +1,6 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12903 가운데 글자 가져오기
+function solution(s) {
+  const isOdd = s.length % 2 === 1;
+  const centerIndex = Math.floor(s.length / 2);
+  return isOdd ? s[centerIndex] : s.substring(centerIndex - 1, centerIndex + 1);
+}
