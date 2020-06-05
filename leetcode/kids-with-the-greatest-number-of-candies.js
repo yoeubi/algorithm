ef61159cdaa7b09c1d512373ac8af9cc7b1e0268
yoeubi <yoeubi28@gmail.com>
2020-06-05 21:51:36 +0900

    oneday algorithm

---
+++ leetcode/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+var kidsWithCandies = function (candies, extraCandies) {
+  const result = [];
+  for (let i = 0; i < candies.length; i++) {
+    const sum = candies[i] + extraCandies;
+    const temp = candies
+      .filter((_, index) => i !== index)
+      .every((candy) => sum >= candy);
+    result.push(temp);
+  }
+  return result;
+};
